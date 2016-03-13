7d919817977b6baf93b1628a8e099bd080824b92
leokmob <lkudryavtsev@mobiquityinc.com>
2016-03-12 20:15:02 -0500

    Added swift version of the background app.

---
+++ SwiftBLEBackground/SwiftBLEBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBLEBackground
+//
+//  Created by Leonid Kudryavtsev on 3/12/16.
+//  Copyright © 2016 Leonid Kudryavtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
